bd261a9498743318de3e871164fb801341be63b1
Maddy798 <hardiktaneja798@gmail.com>
2024-08-21 15:45:06 +0530

    added stripe

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const AuthRoutes = require("./routes/auth");
 const ChatRoomsRoutes = require("./routes/chatrooms");
 const MessagesRoutes = require("./routes/messages");
 const mongoose = require("mongoose");
+const stripe = require("stripe")(
+  "sk_test_51PiTHVGPnsd4Z1tXCr7V1qiKxXtZm5w1DqadxPNixu2VYhxB1DSnaV5LhDtipgfJ9tjUXXJKY47GdVwNyg6wgwvh00l6d53qS6"
+);
 
 const app = express();
 const uri =
@@ -14,9 +17,38 @@ app.use(express.json());
 app.use("/api/auth/", AuthRoutes);
 app.use("/api/chatroom/", ChatRoomsRoutes);
 app.use("/api/messages/", MessagesRoutes);
+app.use("/api/messages/", MessagesRoutes);
 
 app.get("/", (req, res) => {
   res.send({ error: "hehe" });
+});
+
+const calculateOrderAmount = (items) => {
+  let total = 0;
+  items.forEach((item) => {
+    total += item.amount;
+  });
+  return total;
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { amount } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "usd",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  console.log(paymentIntent);
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
 });
 
 mongoose
